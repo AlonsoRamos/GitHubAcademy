d1f1fdee0ef66d096086322f2277b409df42e3d1
AlonsoRamos <alonsoram93@gmail.com>
2017-08-10 14:26:53 -0700

    Imagenes y Load

--- ALOMyCompany/js/Catalog.js
+++ ALOMyCompany/js/Catalog.js
@@ -1,50 +1,16 @@
 //Create a JSON Object my products information
-
-var myCatalog = 
-   {    
-    "MyCollection": {
-        "Product1" : 
-        {
-          "id" : 0,
-          "title" : "myTitle0",
-          "description" : "myDescription",
-          "imgUrl" : "../images/img01.jpg"
-        },
-        "Product2" : 
-        {
-          "id" : 1,
-          "title" : "myTitle1",
-          "description" : "myDescription",
-          "imgUrl" : "../images/img02.jpg"
-        },
-        "Product3" : 
-        {
-          "id" : 2,
-          "title" : "myTitle2",
-          "description" : "myDescription",
-          "imgUrl" : "../images/img03.jpg"
-        }
-    }
-}  
-		
-$(document).ready(function () {
-  //handler for .ready() called.
-  //var test = myCatalog;
-  //alert("Hello");
-
-  //$(myCatalog.MyCollection).each(function (index){
-    //console.log(index + ": " + $(this).text());
-  //});
-  $("#DynamicHTML").append(buildMyProduct());
-
+$(document).ready(function() {
+    $("#DynamicHTML").append(buildMyProduct(MyCatalogData));
 });
 
-function buildMyProduct()
-{ 
-  var html = "<div class='Catalog ProductItem'>";
-  var html = html + "<div>" + myCatalog.MyCollection.Product1.title+"</div>"
-  var html = html + "<div><img class= 'Catalog' src='" + myCatalog.MyCollection.Product1.imgUrl + "' /></div>";
-  var html = html + "<div>"+ myCatalog.MyCollection.Product1.description+"</div>";
-  //var html = + html + "<div>" + MyCatalog.MyCollection.Product1.title+"</div>";
-  return html;
+//Dynamic build your collection
+function buildMyProduct(obj) {
+    var html = "";
+    $.each(obj, function(element, object) {
+        html += "<div class='col-md-2'>";
+        html += "<div><a href='details.html?id=" + object.id + "'>" + object.title + "</a></div>";
+        html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
+        html += "<div>" + object.description + "</div></div>";
+    });
+    return html;
 }
---
+++ ALOMyCompany/js/CatalogData.js
@@ -0,0 +1,39 @@
+//Your jSON Object
+var MyCatalogData = {
+    "Product1": {
+        "id": 0,
+        "title": "myTitle0",
+        "description": "myDescription",
+        "imgUrl": "img/nokia.jpg"
+    },
+    "Product2": {
+        "id": 1,
+        "title": "myTitle1",
+        "description": "myDescription",
+        "imgUrl": "img/nokia2.jpg"
+    },
+    "Product3": {
+        "id": 2,
+        "title": "myTitle2",
+        "description": "myDescription",
+        "imgUrl": "img/nokia3.jpg"
+    },
+    "Product4": {
+        "id": 4,
+        "title": "myTitle0",
+        "description": "myDescription",
+        "imgUrl": "img/nokia4.jpg"
+    },
+    "Product5": {
+        "id": 5,
+        "title": "myTitle1",
+        "description": "myDescription",
+        "imgUrl": "img/nokia5.jpg"
+    },
+    "Product6": {
+        "id": 6,
+        "title": "myTitle2",
+        "description": "myDescription",
+        "imgUrl": "img/nokia6.jpg"
+    }
+}
