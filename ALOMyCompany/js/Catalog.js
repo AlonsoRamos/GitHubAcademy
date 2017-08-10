f0a8c3e929f6037d4d832074bb20e9174d1c1d47
AlonsoRamos <alonsoram93@gmail.com>
2017-08-10 14:41:18 -0700

    update

--- ALOMyCompany/js/Catalog.js
+++ ALOMyCompany/js/Catalog.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
 function buildMyProduct(obj) {
     var html = "";
     $.each(obj, function(element, object) {
-        html += "<div class='col-md-2'>";
-        html += "<div><a href='details.html?id=" + object.id + "'>" + object.title + "</a></div>";
+        html += "<div class='col-md-2 thumbnail'>";
+        html += "<div><a href='Detail2.html?id=" + object.id + "'>" + object.title + "</a></div>";
         html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
         html += "<div>" + object.description + "</div></div>";
     });
--- ALOMyCompany/js/CatalogData.js
+++ ALOMyCompany/js/CatalogData.js
@@ -2,38 +2,38 @@
 var MyCatalogData = {
     "Product1": {
         "id": 0,
-        "title": "myTitle0",
-        "description": "myDescription",
+        "title": "Nokia Ladrillo",
+        "description": "Si se te cae se rompe el suelo.",
         "imgUrl": "img/nokia.jpg"
     },
     "Product2": {
         "id": 1,
-        "title": "myTitle1",
-        "description": "myDescription",
+        "title": "Nokia con luz",
+        "description": "Tiene lus",
         "imgUrl": "img/nokia2.jpg"
     },
     "Product3": {
         "id": 2,
-        "title": "myTitle2",
-        "description": "myDescription",
+        "title": "Nokia mini ladrillo",
+        "description": "Es mini",
         "imgUrl": "img/nokia3.jpg"
     },
     "Product4": {
         "id": 4,
-        "title": "myTitle0",
-        "description": "myDescription",
+        "title": "Nokia de base",
+        "description": "Se recarga en una base.",
         "imgUrl": "img/nokia4.jpg"
     },
     "Product5": {
         "id": 5,
-        "title": "myTitle1",
-        "description": "myDescription",
+        "title": "Nokia base portable",
+        "description": "Portable",
         "imgUrl": "img/nokia5.jpg"
     },
     "Product6": {
         "id": 6,
-        "title": "myTitle2",
-        "description": "myDescription",
+        "title": "Nokia nice",
+        "description": "Tiene tapita",
         "imgUrl": "img/nokia6.jpg"
     }
 }
