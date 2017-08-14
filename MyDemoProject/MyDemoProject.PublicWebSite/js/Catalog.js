8cdcfebf1dd8a63482ad31abd3270a1484a61f2f
AlonsoRamos <alonsoram93@gmail.com>
2017-08-14 14:00:11 -0700

    Bk2

--- MyDemoProject/MyDemoProject.PublicWebSite/js/Catalog.js
+++ MyDemoProject/MyDemoProject.PublicWebSite/js/Catalog.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 function buildMyProduct(obj) {
     var html = "";
     $.each(obj, function(element, object) {
-        html += "<div class='col-md-2 thumbnail'>";
+        html += "<div class='col-md-4 thumbnail'>";
         html += "<div><a href='Detail2.html?id=" + object.id + "'>" + object.title + "</a></div>";
         html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
         html += "<div>" + object.description + "</div></div>";
